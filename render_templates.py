97cc7930a704b17db8dbd4f2e955eae24f64a944
ZeeD26 <zeed.dev@icloud.com>
2016-02-19 20:24:45 +0100

    Add render_templates.py as example serving an actual HTML document.

---
+++ render_templates.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+This is a minimal example of a Flask application that renders an actual HTML
+document. Run it using ::
+
+    python render_templates.py
+
+Open your browser and head to http://127.0.0.1:5000/ and look at the result.
+"""
+from flask import Flask, render_template_string
+app = Flask(__name__)
+
+
+template_string = """
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Hello, World!</title>
+</head>
+<body>
+  <h1>Hello, World!</h1>
+  <p>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+    commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+    velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+    cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+    est laborum.
+  </p>
+</body>
+</html>
+"""
+
+
+@app.route('/')
+def hello_world():
+    return render_template_string(template_string)
+
+
+if __name__ == '__main__':
+    app.run()
